352b113004bd90601b5a4c99182f4d0a928faf6e
Salvador <s_ahumada@ar3d.net>
2020-01-12 15:03:52 -0600

    React app started

---
+++ src/components/counter.jsx
@@ -0,0 +1,11 @@
+// We first import React component
+import React, { Component } from 'react';
+
+class  Counter extends Component {
+    // state = {  }
+    render() { 
+        return <h1>Sup bruh</h1>;
+    }
+}
+ 
+export default Counter ;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
+// This is the entrypoint of our app
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = <h1>Hello World</h1>;
+// First argument is the element we want to render
+// Second element is where we want to render it in our index.hml file
+ReactDOM.render(element, document.getElementById('root'))
