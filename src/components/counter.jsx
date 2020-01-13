80c1913a7dd43ae1ee7eebea885f41f71e1cbee4
Salvador <s_ahumada@ar3d.net>
2020-01-12 20:50:23 -0600

    rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,47 @@
 // We first import React component
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class  Counter extends Component {
-    // state = {  }
-    render() { 
-        return <h1>Sup bruh</h1>;
-    }
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+  render() {
+    return (
+      // React.fragment prevents the parent div to render
+      // You can also use the inline styles directly as an object
+      // Ex. style={ { fontWeight: 'bolder' } }
+      <React.Fragment>
+        <img src={this.state.imageUrl} alt="some random image" />
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        {/* Render lists */}
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+  // Dinamic classes
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 }
- 
-export default Counter ;
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
 const element = <h1>Hello World</h1>;
 // First argument is the element we want to render
 // Second element is where we want to render it in our index.hml file
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(<Counter />, document.getElementById('root'))
