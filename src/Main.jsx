c6ffc00c59e0247396f8d499d951e3597efe4f69
Luis Salvador Ahumada Zepeda <lsaz1790@gmail.com>
2020-07-11 21:11:05 -0500

    Finished table and chart, starting chart img download

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,216 +1,243 @@
 import React, { Component } from "react";
 import Table from "./table";
 import Charts from "./Charts.jsx";
+import Welcome from "./Welcome.js";
 import { ReactComponent as Logo } from "./logo.svg";
 import { Animated } from "react-animated-css";
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            investment: 0,
-            periods: [0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120],
-            interest: 0,
-            monthly: 0,
-            show_result: false,
-            results: [],
-            warning: false,
-            loading: false,
-            type: "table",
-        };
-    }
-
-    componentDidMount() {
-        const that = this;
-        document.getElementById("send-form").onkeydown = e => {
-            if (e.keyCode === 13) {
-                that.startLoading();
-            }
-        };
-    }
-
-    handleChange(e) {
-        this.setState({ investment: e.target.value });
-    }
-
-    handleMonth(e) {
-        this.setState({ monthly: e.target.value });
-    }
-
-    handleInterest(e) {
-        if (e.target.value > 100) return;
-        this.setState({ interest: e.target.value });
-    }
-
-    setLoading = () => {
-        if (this.state.loading === true) {
-            this.setState({ loading: false });
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      investment: 0,
+      periods: [0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120],
+      interest: 0,
+      monthly: 0,
+      show_result: false,
+      results: [],
+      warning: false,
+      loading: false,
+      type: "table",
     };
+  }
 
-    startLoading() {
-        let option = document.getElementById("time_dropdown");
-        let selected_time = option.options[option.selectedIndex].value;
-        let { investment } = this.state;
-        let interest = 1 + this.state.interest / 100 / 12;
+  componentDidMount() {
+    document.getElementById("send-form").onkeydown = (e) => {
+      if (e.keyCode === 13) {
+        this.startLoading();
+      }
+    };
+  }
 
-        if (selected_time === "Meses" || investment === 0 || interest === 1) {
-            this.setState({ warning: true });
-            return;
+  handleChange(e) {
+    this.setState({ investment: e.target.value });
+  }
+
+  handleMonth(e) {
+    this.setState({ monthly: e.target.value });
+  }
+
+  handleInterest(e) {
+    if (e.target.value > 30 || e.target.value < 0) return;
+    this.setState({ interest: e.target.value });
+  }
+
+  setLoading = () => {
+    if (this.state.loading === true) {
+      this.setState({ loading: false });
+    }
+  };
+
+  startLoading() {
+    let option = document.getElementById("time_dropdown");
+    let selected_time = option.options[option.selectedIndex].value;
+    let { investment } = this.state;
+    let interest = 1 + this.state.interest / 100 / 12;
+
+    if (selected_time === "Meses" || investment === 0 || interest === 1) {
+      this.setState({ warning: true });
+      return;
+    }
+
+    this.setState({ loading: true }, this.calculateInvestment());
+  }
+
+  redo() {
+    document.getElementById("time_dropdown").selectedIndex = 0;
+    this.setState({
+      show_result: false,
+      interest: 0,
+      investment: 0,
+      monthly: 0,
+    });
+  }
+
+  getColor(type) {
+    if (type === 1 && this.state.type === "table") return "bolder";
+    if (type === 2 && this.state.type === "chart") return "bolder";
+
+    return "normal";
+  }
+
+  setOption = () => {
+    let type;
+    this.state.type === "table" ? (type = "chart") : (type = "table");
+
+    this.setState({
+      type,
+    });
+  };
+
+  calculateInvestment() {
+    let option = document.getElementById("time_dropdown");
+    let selected_time = option.options[option.selectedIndex].value;
+
+    let total = {};
+    let { investment, monthly } = this.state;
+    let interest = 1 + this.state.interest / 100 / 12;
+
+    let totalArray = [];
+    let initial_investment = investment;
+
+    // Get my monthly return
+    for (let i = 0; i < selected_time; i++) {
+      let month = i + 1;
+      let savings = +initial_investment + +monthly * i;
+
+      if (i !== 0) investment = +investment * +interest;
+      else investment = +investment;
+
+      if (monthly !== 0 && i !== 0) investment = +investment + +monthly;
+
+      if (!total[month]) {
+        total[month] = { investment, savings };
+      }
+    }
+
+    let monthNo = 1;
+    let year = 0;
+
+    for (let element in total) {
+      totalArray.push({ month: total[element], monthNo, year });
+      monthNo++;
+      if (monthNo % 12 === 0) year++;
+    }
+
+    this.setState(
+      {
+        show_result: true,
+        results: totalArray,
+        warning: false,
+        loading: false,
+        type: "table",
+      },
+      // Fallback
+      () => {
+        if (this.state.loading) {
+          this.setState({ loading: false });
         }
+      }
+    );
+  }
 
-        this.setState({ loading: true });
-        setTimeout(() => {
-            this.calculateInvestment();
-        }, 0);
-    }
+  render() {
+    let loading = this.state.loading ? (
+      <div className="loading">
+        <div className="rotate-center">
+          <Logo />
+        </div>
+      </div>
+    ) : (
+      " "
+    );
 
-    redo() {
-        this.setState({ show_result: false });
-        document.getElementById("time_dropdown").selectedIndex = 0;
-    }
+    let chartOrTable =
+      this.state.type === "table" ? (
+        <Table
+          results={this.state.results}
+          redo={() => this.redo()}
+          loadingState={() => this.setLoading()}
+        />
+      ) : (
+        <Charts chartDate={this.state.results} />
+      );
 
-    getColor(type) {
-        if (type === 1 && this.state.type === "table")
-            return "bolder";
-        if (type === 2 && this.state.type === "chart")
-            return "bolder";
+    let table = this.state.show_result ? (
+      <React.Fragment>
+        <div className="button-selection">
+          <button
+            onClick={this.setOption}
+            style={{ fontWeight: this.getColor(1) }}
+          >
+            Tabla
+          </button>
+          <button
+            onClick={this.setOption}
+            style={{ fontWeight: this.getColor(2) }}
+          >
+            Gráfica
+          </button>
+        </div>
+        {chartOrTable}
+      </React.Fragment>
+    ) : (
+      " "
+    );
 
-        return "normal";
-    }
-
-    setOption = () => {
-        let type;
-        this.state.type === "table" ? type = "chart" : type = "table"
-
-        this.setState({
-            type
-        });
-    }
-
-    calculateInvestment() {
-        let option = document.getElementById("time_dropdown");
-        let selected_time = option.options[option.selectedIndex].value;
-
-        let total = {};
-        let { investment, monthly } = this.state;
-        let interest = 1 + this.state.interest / 100 / 12;
-
-        let totalArray = [];
-        let initial_investment = investment;
-
-        // Get my monthly return :)
-        for (let i = 0; i < selected_time; i++) {
-            let month = i + 1;
-            let savings = +initial_investment + +monthly * i;
-
-            if (i !== 0) investment = +investment * +interest;
-            else investment = +investment;
-
-            if (monthly !== 0 && i !== 0) investment = +investment + +monthly;
-
-            if (!total[month]) {
-                total[month] = { investment, savings };
-            }
-        }
-
-        let monthNo = 1;
-        let year = 0;
-
-        for (let element in total) {
-            totalArray.push({ month: total[element], monthNo, year });
-            monthNo++;
-            if (monthNo % 12 === 0) year++
-        }
-
-        this.setState({
-            show_result: true,
-            results: totalArray,
-            interest: 0,
-            investment: 0,
-            monthly: 0,
-            warning: false,
-            loading: false
-        });
-    }
-
-    render() {
-        let loading = this.state.loading ? (
-            <div className="loading">
-                <div className="rotate-center"><Logo /></div>
-            </div>
-        ) : (
-                " "
-            );
-
-        let chartOrTable = this.state.type === "table" ? <Table
-            results={this.state.results}
-            redo={() => this.redo()}
-            loadingState={() => this.setLoading()}
-        /> : <Charts chartDate={this.state.results} />
-
-        let table = this.state.show_result ? (
-            <React.Fragment>
-                <div className="button-selection"><button onClick={this.setOption} style={{ fontWeight: this.getColor(1) }}>Tabla</button><button onClick={this.setOption} style={{ fontWeight: this.getColor(2) }}>Gráfica</button></div>
-                {chartOrTable}
-            </React.Fragment>
-        ) : (
-                " "
-            );
-
-        let warning = this.state.warning
-            ? "Por favor completa todos los campos"
-            : " ";
-        return (
-            <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
-                <div id="send-form">
-                    <h1>iNBERSUS</h1>
-                    <p>1.-Selecciona una inversión inicial</p>
-                    <p className="initial">
-                        <span>$ &nbsp;</span>
-                        <input
-                            type="number"
-                            value={this.state.investment}
-                            onChange={$event => this.handleChange($event)}
-                        />
-                    </p>
-                    <p>2.-Elige un tiempo de inversión.</p>
-                    <p>
-                        <select name="time" id="time_dropdown">
-                            {this.state.periods.map(option => (
-                                <option key={option}>{option === 0 ? "Meses" : option}</option>
-                            ))}
-            ;
-          </select>
-                    </p>
-                    <p>3.-Selecciona tasa de interes anual (1-100)</p>
-                    <p className="interest">
-                        <input
-                            type="number"
-                            value={this.state.interest}
-                            onChange={$event => this.handleInterest($event)}
-                        />
-                        <span>%</span>
-                    </p>
-                    <p>(Opcional) Deposito mensual.</p>
-                    <p className="monthly">
-                        <span>$ &nbsp;</span>
-                        <input
-                            type="number"
-                            value={this.state.monthly}
-                            onChange={$event => this.handleMonth($event)}
-                        />
-                    </p>
-                    <p>
-                        <button onClick={() => this.startLoading()}>CALCULAR</button>
-                    </p>
-                    <p className="warning"> {warning} </p>
-                    {loading}
-                    {table}
-                </div>
-            </Animated>
-        );
-    }
+    let warning = this.state.warning
+      ? "Por favor completa todos los campos"
+      : " ";
+    return (
+      <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
+        <div id="send-form">
+          <h1>iNBERSUS</h1>
+          <p>1.-Selecciona una inversión inicial</p>
+          <p className="initial">
+            <span>$ &nbsp;</span>
+            <input
+              type="number"
+              value={this.state.investment}
+              onChange={($event) => this.handleChange($event)}
+            />
+          </p>
+          <p>2.-Elige un tiempo de inversión.</p>
+          <p>
+            <select name="time" id="time_dropdown">
+              {this.state.periods.map((option) => (
+                <option key={option}>{option === 0 ? "Meses" : option}</option>
+              ))}
+              ;
+            </select>
+          </p>
+          <p>3.-Selecciona tasa de interés anual (1-30)</p>
+          <p className="interest">
+            <input
+              type="number"
+              value={this.state.interest}
+              onChange={($event) => this.handleInterest($event)}
+            />
+            <span>%</span>
+          </p>
+          <p>(Opcional) Deposito mensual.</p>
+          <p className="monthly">
+            <span>$ &nbsp;</span>
+            <input
+              type="number"
+              value={this.state.monthly}
+              onChange={($event) => this.handleMonth($event)}
+            />
+          </p>
+          <p>
+            <button onClick={() => this.startLoading()}>CALCULAR</button>
+          </p>
+          <p className="warning"> {warning} </p>
+          {loading}
+          {table}
+        </div>
+        <Welcome />
+      </Animated>
+    );
+  }
 }
 
 export default Main;
--- src/table.js
+++ src/table.js
@@ -4,116 +4,146 @@ import XLSX from "xlsx";
 import { Animated } from "react-animated-css";
 
 export default function Table(props) {
-    const formatNumber = number => {
-        const options = { style: "currency", currency: "USD" };
-        const numberFormat = new Intl.NumberFormat("en-US", options);
-        return numberFormat.format(number);
-    };
+  const formatNumber = (number) => {
+    const options = { style: "currency", currency: "USD" };
+    const numberFormat = new Intl.NumberFormat("en-US", options);
+    return numberFormat.format(number);
+  };
 
-    const [start, setStart] = useState(0);
+  const [currentYear, setCurrentYear] = useState(1);
 
-    const [end, setEnd] = useState(12);
+  const [animation, setAnimation] = useState(true);
 
-    const [currentYear, setCurrentYear] = useState(1);
+  const [download, setDownload] = useState(false);
 
-    const [animation, setAnimation] = useState(true);
+  const changePage = (type) => {
+    if (type === -1 && currentYear === 1) return;
+    if (type === 1 && currentYear === props.results.length / 12) return;
+    setCurrentYear(currentYear + type);
+  };
 
-    const tablePagination = () => {
-        let currentPage = props.results.slice(start, end);
-        return currentPage;
+  const tablePagination = () => {
+    let year = currentYear * 12;
+    let start = year - 12;
+    let end = year;
+    let currentPage = props.results.slice(start, end);
+
+    // Hacemos otra simulación, reinicia tabla.
+    if (currentPage.length === 0) {
+      currentPage = props.results.slice(0, 12);
+      setCurrentYear(1);
     }
+    return currentPage;
+  };
 
-    useEffect(() => {
-        setTimeout(() => {
-            props.loadingState();
-        }, 0);
+  useEffect(() => {
+    setTimeout(() => {
+      props.loadingState();
+    }, 0);
+  });
 
-        // let header = document.getElementById("inline-table");
-        // let fixed = document.getElementById("fix-table");
-        // window.addEventListener("scroll", function (e) {
-        //     if (window.scrollY - 500 > header.getBoundingClientRect().top) {
-        //         fixed.style.display = "inline";
-        //         fixed.style.position = "fixed";
-        //         fixed.style.top = "0";
-        //         fixed.style.left = "50%";
-        //         fixed.style.transform = "translate(-50%,0)";
-        //     } else {
-        //         fixed.style.display = "none";
-        //     }
-        // });
-    });
+  const downloadTable = () => {
+    setDownload(true);
+    setTimeout(() => {
+      document.body.style.cursor = "wait";
+      var downloadTime = new Date();
+      var day = downloadTime.getDate();
+      var month = downloadTime.getMonth() + 1;
+      var year = downloadTime.getFullYear();
+      var dom = document.getElementById("download_xlsx");
+      const wb = XLSX.utils.table_to_book(dom, { sheet: "inbersus" });
+      document.body.style.cursor = "auto";
+      setDownload(false);
+      return XLSX.writeFile(wb, `inbersus_${day}_${month}_${year}.xlsx`);
+    }, 0);
+  };
 
-    const downloadTable = () => {
-        document.body.style.cursor = "wait";
-        var downloadTime = new Date();
-        var day = downloadTime.getDate();
-        var month = downloadTime.getMonth() + 1;
-        var year = downloadTime.getFullYear();
-        var dom = document.getElementById("download-table");
-        const wb = XLSX.utils.table_to_book(dom, { sheet: "inbersus" });
-        document.body.style.cursor = "auto";
-        return XLSX.writeFile(wb, `inbersus_${day}_${month}_${year}.xlsx`);
-    };
+  const isVisible = () => {
+    return animation;
+  };
 
-    const isVisible = () => {
-        return animation;
-    }
+  const redoInvestment = () => {
+    setAnimation(false);
+    setTimeout(() => {
+      props.redo();
+    }, 1000);
+  };
 
-    const redoInvestment = () => {
-        setAnimation(false)
-        setTimeout(() => {
-            props.redo();
-        }, 1000);
-    };
+  const download_xslx = download ? (
+    <table id="download_xlsx">
+      <tbody>
+        <thead>
+          <tr>
+            <th>Mes</th>
+            <th>Cantidad ahorrada</th>
+            <th>Inversion Total</th>
+            <th>Retorno</th>
+          </tr>
+        </thead>
+        {props.results.map((month, index) => (
+          <tr key={index}>
+            <th>{month.monthNo}</th>
+            <th>{formatNumber(month.month.savings)}</th>
+            <th>{formatNumber(month.month.investment)}</th>
+            <th>
+              {formatNumber(month.month.investment - month.month.savings)}
+            </th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ) : (
+    " "
+  );
 
-    return (
-        <div className="table-wrapper">
-            {/* <table>
-                <thead id="fix-table">
-                    <tr className="head">
-                        <th>Año</th>
-                        <th>Mes</th>
-                        <th>Cantidad ahorrada</th>
-                        <th>Inversion Total</th>
-                        <th>Retorno</th>
-                    </tr>
-                </thead>
-            </table> */}
-            <table id="download-table">
-                <thead id="inline-table">
-                    <tr className="head">
-                        {/* <th>Año</th> */}
-                        <th>Mes</th>
-                        <th>Cantidad ahorrada</th>
-                        <th>Inversion Total</th>
-                        <th>Retorno</th>
-                    </tr>
-                </thead>
-                <tbody className="body">
-                    {tablePagination().map((month, index) => (
-                        <tr key={index}>
-                            {/* <th>{month.monthNo % 12 === 0 ? month.year : ''}</th> */}
-                            <th>{month.monthNo}</th>
-                            <th>{formatNumber(month.month.savings)}</th>
-                            <th>{formatNumber(month.month.investment)}</th>
-                            <th>
-                                {formatNumber(month.month.investment - month.month.savings)}
-                            </th>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div class="controllers"> prev Año {currentYear} next </div>
-            <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={isVisible()}>
-                <div className="download">
-                    <button onClick={redoInvestment}>
-                        Crear otra simulación <i className="material-icons">cached</i>
-                    </button>
-                    <button onClick={downloadTable}>
-                        Descargar XLSX <i className="material-icons">file_download</i>
-                    </button>
-                </div>
-            </Animated>
+  return (
+    <div className="table-wrapper">
+      <table id="download-table">
+        <thead id="inline-table">
+          <tr className="head">
+            <th>Mes</th>
+            <th>Cantidad ahorrada</th>
+            <th>Inversion Total</th>
+            <th>Retorno</th>
+          </tr>
+        </thead>
+        <tbody className="body">
+          {tablePagination().map((month, index) => (
+            <tr key={index}>
+              <th>{month.monthNo}</th>
+              <th>{formatNumber(month.month.savings)}</th>
+              <th>{formatNumber(month.month.investment)}</th>
+              <th>
+                {formatNumber(month.month.investment - month.month.savings)}
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="controllers">
+        <button onClick={() => changePage(-1)}>
+          <i className="material-icons">chevron_left</i>
+        </button>{" "}
+        <span>Año {currentYear}</span>
+        <button onClick={() => changePage(1)}>
+          <i className="material-icons">chevron_right</i>
+        </button>{" "}
+      </div>
+      {download_xslx}
+      <Animated
+        animationIn="fadeIn"
+        animationOut="fadeOut"
+        isVisible={isVisible()}
+      >
+        <div className="download">
+          <button onClick={redoInvestment}>
+            Crear otra simulación <i className="material-icons">cached</i>
+          </button>
+          <button onClick={downloadTable}>
+            Descargar XLSX <i className="material-icons">file_download</i>
+          </button>
         </div>
-    );
+      </Animated>
+    </div>
+  );
 }
